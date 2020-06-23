f22fec9ef9d98c41c0873e9101ed955df7cd731b
kevvkevvi <54913154+kevvkevvi@users.noreply.github.com>
2020-06-22 21:32:49 -0400

    Add files via upload

--- maven_Test/src/main/java/com/motif/controller/HelloSpringBoot.java
+++ maven_Test/src/main/java/com/motif/controller/HelloSpringBoot.java
@@ -1,1 +1,15 @@
+package com.motif.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+public class HelloSpringBoot {
+
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "hello SpringBoot";
+	}
+}
