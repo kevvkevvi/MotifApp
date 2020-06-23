7fb3c8b51cfb35b4e2908122845eccbe5685f9af
kevvkevvi <54913154+kevvkevvi@users.noreply.github.com>
2020-06-22 21:11:10 -0400

    Add files via upload

--- maven_Test/src/main/java/com/motif/SpringBootApplications.java
+++ maven_Test/src/main/java/com/motif/SpringBootApplications.java
@@ -1,1 +1,13 @@
+package com.motif;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplications {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplications.class, args);
+	}
+	
+}
