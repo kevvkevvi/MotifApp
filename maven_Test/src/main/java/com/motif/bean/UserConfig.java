bc62a496bd4ac1877eb36427709f12724f932539
kevvkevvi <54913154+kevvkevvi@users.noreply.github.com>
2020-06-22 21:12:45 -0400

    Add files via upload

--- maven_Test/src/main/java/com/motif/bean/UserConfig.java
+++ maven_Test/src/main/java/com/motif/bean/UserConfig.java
@@ -1,1 +1,16 @@
+package com.motif.bean;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.motif.common.*;
+
+@Configuration
+@ComponentScan(basePackageClasses = User.class)
+public class UserConfig {
+	@Bean
+    public Profile getProfile() {
+        return new Profile("Kevin Li", 21, 1);
+    }
+}
